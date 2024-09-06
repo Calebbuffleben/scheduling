a454b4a333677c4123ac887f619d2063314b3b05
Calebbuffleben <calebbuffleben@gmail.com>
2024-09-06 17:11:29 -0300

    removing trash

--- frontend/src/components/CompanyRegistration/CompanyRegistrationComponent.tsx
+++ frontend/src/components/CompanyRegistration/CompanyRegistrationComponent.tsx
@@ -29,55 +29,6 @@ const CompanyRegistrationComponent = ({ onSubmit, handleSubmit, register, errors
                     errors={errors} 
                     inputs={inputs} 
                 />
-               {/* <form onSubmit={handleSubmit(onSubmit)}>
-    
-                    <div>
-                        <input 
-                            type="text" 
-                            placeholder="Company Name"
-                            {...register('companyName', {required: 'Company name is required'})}
-                        />
-                        {errors.companyName && <span>{errors.companyName.message}</span>}
-                    </div>
-                    <div>
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            {...register('email', {
-                                required: 'Email is required',
-                                pattern: {
-                                    value: /^\S+@\S+$/i,
-                                    message: 'Enter a valid email address'
-                                }
-                            })}
-                        />
-                        {errors.email && <span>{errors.email.message}</span>}
-                    </div>
-
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Address"
-                            {...register('address', {required: 'Adress is required'})}
-                        />
-                        {errors.address && <span>{errors.address.message}</span>}
-                    </div>
-                    <div>
-                    <input
-                            type="tel"
-                            placeholder="Phone Number"
-                            {...register('phoneNumber', {
-                                required: 'Phone Number is required',
-                                pattern: {
-                                    value: /^\d{10,15}$/,
-                                    message: 'Enter a valid phone number'
-                                }
-                            })}
-                        />
-                        {errors.phoneNumber && <span>{errors.phoneNumber.message}</span>} 
-                    </div>
-                    <button type="submit">Register Company</button>
-                </form> */}
             </ContainerRight>
         </Right>
     </Container>
