4372baef84b2e2b9d4120608b496f578a7bf7f41
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-07 18:30:16 -0300

    company registration page test

---
+++ frontend/src/__tests__/pages/CompanyRegistrationPage.spec.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import CompanyRegistrationPage from '../../pages/Registers/company-register';
+import { render, screen } from '@testing-library/react';
+import FormComponent from '../../components/ui/FormComponent/FormComponent';
+
+describe("Handle with api and http responses", () => {
+    test("", () => {
+        render()
+    })
+})
