c0943e82d2b9800757375a265bc137b994153343
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-08 10:50:02 -0300

    add tests

--- frontend/src/__tests__/pages/CompanyRegistrationPage.spec.tsx
+++ frontend/src/__tests__/pages/CompanyRegistrationPage.spec.tsx
@@ -1,10 +1,16 @@
 import '@testing-library/jest-dom';
 import CompanyRegistrationPage from '../../pages/Registers/company-register';
-import { render, screen } from '@testing-library/react';
-import FormComponent from '../../components/ui/FormComponent/FormComponent';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe("Handle with api and http responses", () => {
-    test("", () => {
+    test("The function is being called when click at the button", () => {
+        const handleClick = jest.fn();
+
         render( <CompanyRegistrationPage />)
+
+        const button = screen.getByText("Register Company")
+        fireEvent.click(button)
+
+        expect(handleClick).toHaveBeenCalledTimes(1);
     })
 })
