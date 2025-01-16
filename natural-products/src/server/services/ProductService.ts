b9b1936ddc6f10ca48cd521cba1962fe2860193e
Calebbuffleben <calebbuffleben@gmail.com>
2025-01-16 11:26:04 -0300

    product service

--- natural-products/src/server/services/ProductService.ts
+++ natural-products/src/server/services/ProductService.ts
@@ -1,7 +1,26 @@
-const ProductService = () => {
-    const getProducts = async () => {
-        // The implementation of this function is not important for this example
-    };
-    return { getProducts };
+export const ProductService = {
+    async getAllProducts() {
+        return await ProductRepository.findAll();
+    },
+    async getProductsById(id: string) {
+        const product = await ProductRepository.findById(id);
+        if (!product) {
+            throw new Error("Product not found");
+        }
+        return product;
+    },
+    async createProduct(data: any) {
+        if (data.price < 0) {
+            throw new Error('Price cannot be negative');
+        }
+      
+        return await ProductRepository.create(data);
+    },
+    async updateProduct(id: string, data: any) {
+        if(data.price < 0) {
+            throw new Error('Price cannot be negative');
+        }
+        return await ProductRepository.update(id, data);
+    }
 };
 export default { ProductService };
