0c69c1db39a32db8fa637fc9ee32e1b0b699d9c1
Calebbuffleben <calebbuffleben@gmail.com>
2025-02-06 17:40:12 -0300

    add endpoint

--- natural-products/src/pages/inventory/register-product/index.tsx
+++ natural-products/src/pages/inventory/register-product/index.tsx
@@ -1,10 +1,11 @@
 import ProductForm from "@/components/Inventory/ProductFormComponent/ProductFormComponent";
 import { IProductFormInput } from "@/interfaces/IProductFormInput";
-
+import api from "@/api-connection/service";
 
 const RegisterProductPage = () => {
-    const handleProductSubmit = (data: IProductFormInput) => {
-        console.log('Form Submitted:', data);
+    const handleProductSubmit = async (data: IProductFormInput) => {
+      console.log('Form Submitted:', data);
+      const response = await api.post('/products', data);
     };
     return (      
           <ProductForm
