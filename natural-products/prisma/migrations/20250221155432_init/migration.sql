dad2c1472aa7a8cbc17a912ef812715ee126844b
Calebbuffleben <calebbuffleben@gmail.com>
2025-02-21 12:55:19 -0300

    add fields to the table

--- natural-products/prisma/migrations/20250221155432_init/migration.sql
+++ natural-products/prisma/migrations/20250221155432_init/migration.sql
@@ -2,8 +2,10 @@
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
+    "sku" TEXT NOT NULL,
+    "price" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
