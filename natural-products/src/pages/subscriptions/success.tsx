6eb35dfc3a9fd9155a4022ac9766f040595bcd57
Calebbuffleben <calebbuffleben@gmail.com>
2025-03-28 15:46:43 -0300

    improving the code

--- natural-products/src/pages/subscriptions/success.tsx
+++ natural-products/src/pages/subscriptions/success.tsx
@@ -9,20 +9,20 @@ const SubscriptionSuccessPage = () => {
   const { session_id } = router.query;
 
   useEffect(() => {
-    const updateSubscription = async () => {
-      if (session_id) {
-        try {
-          await api.post('/subscriptions/update-subscription', { sessionId: session_id });
-          router.push('/dashboard');
-        } catch (error) {
-          console.error('Error updating subscription:', error);
-          // Handle the error appropriately
-        }
-      }
-    };
-
     updateSubscription();
   }, [session_id]);
+
+  const updateSubscription = async () => {
+    if (session_id) {
+      try {
+        await api.post('/subscriptions/update-subscription', { sessionId: session_id });
+        router.push('/dashboard');
+      } catch (error) {
+        console.error('Error updating subscription:', error);
+        // Handle the error appropriately
+      }
+    }
+  };
 
   return <div>Processing subscription...</div>;
 };
